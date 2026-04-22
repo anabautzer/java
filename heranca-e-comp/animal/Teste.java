3e25fcf8f76bfedb3b8762b329bfffc1f8f06af9
Ana Carolina Bautzer <ac.bautzer@hotmail.com>
2026-04-22 11:04:02 -0300

    Add Ave instance and print its info

--- heranca-e-comp/animal/Teste.java
+++ heranca-e-comp/animal/Teste.java
@@ -3,5 +3,7 @@ public class Teste {
     public static void main(String[] args) {
     Mamifero m = new Mamifero("Ana", 19, "Humano", 500.9, 9);
         System.out.println(m.getInfo());
+    Ave a = new Ave("Abroba", 5, "Ave", 540.9, 500);
+        System.out.println(a.getInfo());
     }
 }
